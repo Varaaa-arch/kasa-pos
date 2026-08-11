0bc139782d121a820413735e613cf1b7faf9b1b8
Varaaa-arch <mahesvara1810@gmail.com>
2026-08-11 20:09:17 +0700

    feat(receipt): add receipt data model

---
+++ internal/printer/receipt/model.go
@@ -0,0 +1,50 @@
+package receipt
+
+import "time"
+
+type Receipt struct {
+	Store       Store
+	Transaction Transaction
+	Items       []Item
+	Summary     Summary
+	Payment     Payment
+	Footer      Footer
+}
+
+type Store struct {
+	Name    string
+	Address string
+	Phone   string
+}
+
+type Transaction struct {
+	InvoiceNumber string
+	TimeStamp     time.Time
+	Cashier       string
+}
+
+type Item struct {
+	Name      string
+	SKU       string
+	Quantity  int
+	UnitPrice int64
+	SubTotal  int64
+}
+
+type Summary struct {
+	SubTotal      int64
+	Discount      int64
+	Tax           int64
+	ServiceCharge int64
+	Total         int64
+}
+
+type Payment struct {
+	Method string
+	Paid   int64
+	Change int64
+}
+
+type Footer struct {
+	Message string
+}
